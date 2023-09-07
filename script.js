67f25832bba2a8d65c6be418e2b756fb7782ff35
Akay <akaudeh@gmail.com>
2023-09-07 09:27:47 +0100

    🎨 add test questions

--- script.js
+++ script.js
@@ -18,30 +18,104 @@ var score=0;
 
 var questions=[
     {
-        q:'Which of these is not an ocean?',
-        options:['Atlantic ocean', 'Oceania', 'Indian ocean'],
-        answer: 1
-    },
-    {
-        q:'Which of these is not a programming language?',
-        options:['JavaScript', 'CSS', 'Ruby'],
-        answer: 1
-    },
-    {
-        q:'What is the capital city of Spain?',
-        options:['Madrid', 'Barcelona', 'Maldives'],
+        q:'What does HTML stand for?',
+        options:[
+            'A: Hyper Text Markup Language', 
+            'B: High Tech Machine Learning', 
+            'C: Hyper Transfer Markup Language', 
+            'D: Href TypeDocument Markup Language'],
         answer: 0
     },
     {
-        q:'Who played Neo in The Matrix?',
-        options:['Jason Statham', 'Chris Hemsworth', 'Keanu Reeves'],
+        q:'What is the purpose of HTML in web development?',
+        options:[
+        'A: To create advanced animations',
+        'B: To design complex databases',
+        'C: To structure and present content on the web',
+        'D: To provide backend server functionality.'
+        ],
         answer: 2
     },
     {
-        q:'What year was the Titanic built?',
-        options:['1911', '1920', '1924'],
+        q:'How do you create a heading level 2 (h2) element in HTML?',
+        options:[
+            'A:  <heading2>Heading Text</heading2>',
+            'B:  <h2>Heading Text</h2>',
+            'C:  <h2>Text Heading</h2>',
+            'D:  <h2>Text</h2>'
+        ],
+        answer: 1
+    },
+    {
+        q:'Which HTML tag is used to create an ordered list?',
+        options:[
+            'A:  <ul>',
+            'B:  <li>',
+            'C:  <ol>',
+            'D:  <dl>'
+        ],
+        answer: 2
+    },
+    {
+        q:'How do you add a link to another website in HTML?',
+        options:[
+            'A:  <a source="https://www.example.com">Visit Example</a>',
+            'B:  <link href="https://www.example.com">Visit Example</link>',
+            'C:  <a href="https://www.example.com">Visit Example</a>',
+            'D:  <url>https://www.example.com</url>'
+        ],
+        answer: 2
+    },
+    {
+        q:'What HTML element is used to display an image?',
+        options:[
+            'A:  <img>',
+            'B:  <picture>',
+            'C:  <image>',
+            'D:  <src>'
+        ],
         answer: 0
-    }
+    },
+    {
+        q:'What is the HTML tag for creating a line break?',
+        options:[
+            'A:  <br>',
+            'B:  <lb>',
+            'C:  <break>',
+            'D:  <newline>'
+        ],
+        answer: 0
+    },
+    {
+        q:'How can you create a comment in an HTML document?',
+        options:[
+            'A:  <!-- This is a comment -->',
+            'B:  // This is a comment',
+            'C:  # This is a comment',
+            'D:  /* This is a comment */'
+        ],
+        answer: 0
+    },
+    {
+        q:'Which HTML tag is used to define a paragraph?',
+        options:[
+            'A:  <paragraph>',
+            'B:  <p>',
+            'C:  <para>',
+            'D:  <text>'
+        ],
+        answer: 1
+    },
+    {
+        q:'What does the HTML  <em>  tag represent?',
+        options:[
+            'A: Embedded content',
+            'B: Email address',
+            'C: Emphasized text',
+            'D: Error message'
+        ],
+        answer: 2
+    },
 ]
 
 totalQuestionSpan.innerHTML=questions.length;
--- testScript.js
+++ testScript.js
@@ -8,6 +8,7 @@ var question = document.querySelector(".question");
 var op1 = document.querySelector(".option1");
 var op2 = document.querySelector(".option2");
 var op3 = document.querySelector(".option3");
+var op4 = document.querySelector(".option4");
 var quizContainer = document.querySelector(".start-quiz");
 var quizOverContainer = document.querySelector(".quiz-over");
 var questionIndex;
@@ -18,31 +19,105 @@ var score = 0;
 
 var questions=[
     {
-        q:'Which of these is not an ocean?',
-        options:['Atlantic ocean', 'Oceania', 'Indian ocean'],
-        answer: 1
-    },
-    {
-        q:'Which of these is not a programming language?',
-        options:['JavaScript', 'CSS', 'Ruby'],
-        answer: 1
-    },
-    {
-        q:'What is the capital city of Spain?',
-        options:['Madrid', 'Barcelona', 'Maldives'],
+        q:'What does HTML stand for?',
+        options:[
+            'A: Hyper Text Markup Language', 
+            'B: High Tech Machine Learning', 
+            'C: Hyper Transfer Markup Language', 
+            'D: Href TypeDocument Markup Language'],
         answer: 0
     },
     {
-        q:'Who played Neo in The Matrix?',
-        options:['Jason Statham', 'Chris Hemsworth', 'Keanu Reeves'],
+        q:'What is the purpose of HTML in web development?',
+        options:[
+        'A: To create advanced animations',
+        'B: To design complex databases',
+        'C: To structure and present content on the web',
+        'D: To provide backend server functionality.'
+        ],
         answer: 2
     },
     {
-        q:'What year was the Titanic built?',
-        options:['1911', '1920', '1924'],
+        q:'How do you create a heading level 2 (h2) element in HTML?',
+        options:[
+            'A:  &lt;heading2&gt;Heading Text&lt;/heading2&gt;',
+            'B:  &lt;h2&gt;Heading Text&lt;/h2&gt;',
+            'C:  &lt;h2&gt;Text Heading&lt;/h2&gt;',
+            'D:  &lt;h2&gt;Text&lt;/h2&gt;'
+        ],
+        answer: 1
+    },
+    {
+        q:'Which HTML tag is used to create an ordered list?',
+        options:[
+            'A:  &lt;ul&gt;',
+            'B:  &lt;li&gt;',
+            'C:  &lt;ol&gt;',
+            'D:  &lt;dl&gt;'
+        ],
+        answer: 2
+    },
+    {
+        q:'How do you add a link to another website in HTML?',
+        options:[
+            'A:  &lt;a source="https://www.example.com"&gt;Visit Example&lt;/a&gt;',
+            'B:  &lt;link href="https://www.example.com"&gt;Visit Example&lt;/link&gt;',
+            'C:  &lt;a href="https://www.example.com"&gt;Visit Example&lt;/a&gt;',
+            'D:  &lt;url&gt;https://www.example.com&lt;/url&gt;'
+        ],
+        answer: 2
+    },
+    {
+        q:'What HTML element is used to display an image?',
+        options:[
+            'A:  &lt;img&gt;',
+            'B:  &lt;picture&gt;',
+            'C:  &lt;image&gt;',
+            'D:  &lt;src&gt;'
+        ],
         answer: 0
-    }
-  ]
+    },
+    {
+        q:'What is the HTML tag for creating a line break?',
+        options:[
+            'A:  &lt;br&gt;',
+            'B:  &lt;lb&gt;',
+            'C:  &lt;break&gt;',
+            'D:  &lt;newline&gt;'
+        ],
+        answer: 0
+    },
+    {
+        q:'How can you create a comment in an HTML document?',
+        options:[
+            'A:  &lt;!-- This is a comment --&gt;',
+            'B:  // This is a comment',
+            'C:  # This is a comment',
+            'D:  /* This is a comment */'
+        ],
+        answer: 0
+    },
+    {
+        q:'Which HTML tag is used to define a paragraph?',
+        options:[
+            'A:  &lt;paragraph&gt;',
+            'B:  &lt;p&gt;',
+            'C:  &lt;para&gt;',
+            'D:  &lt;text&gt;'
+        ],
+        answer: 1
+    },
+    {
+        q:'What does the HTML &lt;em&gt; tag represent?',
+        options:[
+            'A: Embedded content',
+            'B: Email address',
+            'C: Emphasized text',
+            'D: Error message'
+        ],
+        answer: 2
+    },
+]
 
 totalQuestionSpan.innerHTML = questions.length;
 
@@ -52,6 +127,7 @@ function load() {
   op1.innerHTML = questions[questionIndex].options[0];
   op2.innerHTML = questions[questionIndex].options[1];
   op3.innerHTML = questions[questionIndex].options[2];
+  op4.innerHTML = questions[questionIndex].options[3];
   index++;
 }
 
